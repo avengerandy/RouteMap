169f7b183d2ba3f1621db1ccc6fc42fbeb5afa5a
MazeR <mazer0701@gmail.com>
2018-02-19 00:07:07 +0800

    remove db class

--- application/Index.php
+++ application/Index.php
@@ -4,7 +4,6 @@ namespace Application;
 
 use Flight;
 use RouteMap\Core\Application;
-use RouteMap\Core\DBFactory;
 
 class Index extends Application {
     public function router() {
@@ -12,22 +11,10 @@ class Index extends Application {
             echo 'hello, world';
         });
         Flight::route('/var/@var', function($var){
-            echo "get var = $var";
+            echo 'get var = ' . $var;
         });
         Flight::route('/redirect', function() {
-            Flight::redirectOutside('/application2/var/456');
-        });
-        Flight::route('/db', function() {
-            $db = DBFactory::getDB([
-                'database_type' => 'mysql',
-                'database_name' => 'event200_anlong',
-                'server' => 'localhost',
-                'username' => 'root',
-                'password' => '',
-                'charset' => 'utf8'
-            ]);
-            $datas = $db->select("news", '*');
-            var_dump($datas);
+            Flight::redirectOutside('/application2/var/redirectVar');
         });
     }
 }
